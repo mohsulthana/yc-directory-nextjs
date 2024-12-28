2dac174d86fa374bb8a273f7a67ebcae7f81eb45
Mohammad Sulthan <muhammadsultana@outlook.com>
2024-12-28 14:52:57 +0700

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,55 @@
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+    searchParams,
+}: Readonly<{
+    searchParams: Promise<{ query?: string }>;
+}>) {
+    const query = (await searchParams).query;
+
+    const posts = [
+        {
+            _createdAt: new Date(),
+            views: 55,
+            author: { _id: 1, name: 'Sulthan' },
+            _id: 1,
+            description: "This is a description",
+            image: "https://images.unsplash.com/photo-1527430253228-e93688616381?q=80&w=2668&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            category: "Robots",
+            title: "We Robots",
+        },
+    ];
+
+    return (
+        <>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Pitch Your Startup, <br /> Connect with Entrepreneurs
+                </h1>
+
+                <p className="sub-heading !max-w-3xl">
+                    Submit Ideas, Vote on Pitches, and Connect with Founders
+                </p>
+
+                <SearchForm query={query} />
+            </section>
+
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for ${query}` : "All Startups"}
+                </p>
+
+                <ul className="mt-7 card_grid">
+                    {posts.length > 0 ? (
+                        posts.map((post: StartupCardType, index: number) => (
+                            <StartupCard key={post?.id} post={post} />
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found</p>
+                    )}
+                </ul>
+            </section>
+        </>
+    );
+}

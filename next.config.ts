3481bba656c2f86dd61dbf0022bc6d04b302f1e9
Mohammad Sulthan <muhammadsultana@outlook.com>
2025-01-04 15:52:58 +0700

    ignore ts and eslint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
@@ -11,7 +17,7 @@ const nextConfig: NextConfig = {
     ]
   },
   experimental: {
-    ppr: 'incremental'
+    ppr: 'incremental',
   },
   devIndicators: {
     appIsrStatus: true,
